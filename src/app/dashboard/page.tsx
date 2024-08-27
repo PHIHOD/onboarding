423f2b72453cfdb1fe6b784dd7e40de7b80ccf5b
PHIHOD <60650542+PHIHOD@users.noreply.github.com>
2024-08-28 01:33:44 +0200

    margin bottom 48

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Dashboard() {
   return (
     <div className="container">
-      <header className="dashboard-header">
+      <header className="dashboard-header mb-48">
         <h1 className="text-display-2">Welcome to Midpilot Docs</h1>
-        <h2>Stay tuned. Docs will be available soon.</h2>
+        <h2 className="text-body-1">Stay tuned. Docs will be available soon.</h2>
       </header>
       
       
